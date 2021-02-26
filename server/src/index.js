3f9329638e65d190cd88230116090cf86aed726f
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-02-26 14:26:36 +1100

    update server/index.js - examples from movie app

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,3 @@
-console.log("Event marketplace app is running");
-
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
@@ -9,9 +7,11 @@ const internalRouter = require("./routes/internalRoutes");
 const hireItemRouter = require("./routes/hireItemRoutes");
 const userRouter = require("./routes/userRoutes");
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose.connect("mongodb://localhost/HireItems", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 const app = express();
@@ -32,6 +32,6 @@ app.use("/internal", internalRouter);
 app.use("/api/hire-items", hireItemRouter);
 app.use("/api/user", userRouter);
 
-app.listen(port, () =>
-  console.log(`event app is listening at http://localhost:${port}"`)
-);
+app.listen(process.env.PORT || 9000, () => {
+  console.log('listening on port 9000');
+});
