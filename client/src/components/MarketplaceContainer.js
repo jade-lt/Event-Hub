a0871fb755d022bf57cd4e21415806f21f24572d
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-02-26 13:58:59 +1100

    updated marketplace container to render all

--- client/src/components/MarketplaceContainer.js
+++ client/src/components/MarketplaceContainer.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { DashboardContainer } from "./DashboardContainer";
 import { UserLogin } from "./UserLogin";
+import { UserRegister } from "./UserRegister";
 
 const MarketplaceContainer = () => {
     return (
         <div>
+            <h1 class="header-text">The Event Marketplace</h1>
             <UserLogin />
+            <UserRegister />
+            <DashboardContainer />
         </div>
         
     )
