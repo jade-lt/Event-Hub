7cafbd04658ec95e78c7e9528cb22887a0aa98de
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-03-03 13:11:42 +1100

    edits to css and user listings

--- client/src/components/UserListings.js
+++ client/src/components/UserListings.js
@@ -5,12 +5,15 @@ const UserListings = (props) => {
     return (
       <ul>
         {props.listings.map((el, index) => (
+          <div class="user-items-list">
         <li key={index} onClick={() => props.handleClick(index)}>
           <img class="item-img"src={el.image}></img>
-             Name: {el.name} // Color: {el.color} // Size: {el.size} // Amount: {el.amount} // Cost: {el.cost} // Image: {el.image}
+             Name: {el.name} // Color: {el.color} // Size: {el.size} // Amount: {el.amount} // Cost: {el.cost} 
           </li>
+          </div>
         ))}
       </ul>
+      
     );
   };
 
